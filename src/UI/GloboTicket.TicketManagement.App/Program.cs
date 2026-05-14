940b24e171553d9bdaa0a2ce9687b3333376d980
Ruslan <ruslant994@gmail.com>
2026-05-14 15:02:07 +0300

    Update API base addresses to production URL

--- src/UI/GloboTicket.TicketManagement.App/Program.cs
+++ src/UI/GloboTicket.TicketManagement.App/Program.cs
@@ -22,12 +22,12 @@ builder.Services.AddScoped<AuthenticationStateProvider, CookieAuthenticationStat
 
 builder.Services.AddTransient<CookieAuthenticationStateProvider>();
 
-builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri("https://localhost:7088/"))
+builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri("https://globotickeapi-erdmabafchcagzac.westeurope-01.azurewebsites.net/"))
     .AddHttpMessageHandler<CookieHandler>();
 
 builder.Services.AddHttpClient(
     "Authentication",
-    client => client.BaseAddress = new Uri("https://localhost:7088"))
+    client => client.BaseAddress = new Uri("https://globotickeapi-erdmabafchcagzac.westeurope-01.azurewebsites.net/"))
     .AddHttpMessageHandler<CookieHandler>();
 
 builder.Services.AddScoped<IEventDataService, EventDataService>();
